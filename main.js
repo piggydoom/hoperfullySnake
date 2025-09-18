9a2632dfdfd6eb4a17baa754b247e2d33afc0617
artist piggy <artistpiggy@gmail.com>
2025-09-18 10:30:28 +1000

    top and left bug

--- main.js
+++ main.js
@@ -1,45 +1,53 @@
-const field = document.getElementById('playField');
-const ctx = field.getContext('2d');
-ctx.fillStyle = '#007bff';
-ctx.fillRect(200, 200, 10, 10); 
-var posX = 200;
-var posY = 200;
-let direction = null;
+  const field = document.getElementById('playField');
+  const ctx = field.getContext('2d');
+  const cellSize = 10; 
+  ctx.fillStyle = '#007bff';
+  ctx.fillRect(200, 200, 10, 10);
+  var posX = (field.clientWidth / 2);
+  var posY = (field.clientWidth / 2);
 
-function moveSnake(direction){
-ctx.clearRect(10, 10, posX, posY);
+  let direction = null;
 
-if(direction === null){
+  function moveSnake(direction){
 
-} else{
-if(direction === "up"){
-  posY -= 10;
-  ctx.fillRect(posX, posY, 10, 10);
-};
+  
+  
+    ctx.clearRect(cellSize, cellSize, posX, posY);
+  
+  if(direction === "up"){
+    posY -= 10;
+    ctx.fillRect(posX, posY, 10, 10);
+  };
 
-if(direction === "down"){
-  posY += 10;
-  ctx.fillRect(posX, posY, 10, 10);
-};
+  if(direction === "down"){
+    posY += 10;
+    ctx.fillRect(posX, posY, 10, 10);
+  };
 
-if(direction === "left"){
-  posX -= 10;
-  ctx.fillRect(posX, posY, 10, 10);
-};
+  if(direction === "left"){
+   
+    posX -= 10;
+    ctx.fillRect(posX, posY, 10, 10);
+  };
 
-if(direction === "right"){
-  posX += 10;
-  ctx.fillRect(posX, posY, 10, 10);
-};
-}};
+  if(direction === "right"){
+    posX += 10;
+    ctx.fillRect(posX, posY, 10, 10);
+  };
 
-document.addEventListener("keypress", function(event) {
+  
 
-  if (event.key === "w") { direction = "up"; }
-  if (event.key === "s") { direction = "down"; }
-  if (event.key === "a") { direction = "left"; }
-  if (event.key === "d") { direction = "right"; }
+  console.log(posX + "-" + posY)
+  };
 
-});
+  document.addEventListener("keypress", function(event) {
 
-setInterval(() => moveSnake(direction), 200);
+    if (event.key === "w") { direction = "up"; }
+    if (event.key === "s") { direction = "down"; }
+    if (event.key === "a") { direction = "left"; }
+    if (event.key === "d") { direction = "right"; }
+
+  });
+
+  setInterval(() => 
+    moveSnake(direction), 200);
