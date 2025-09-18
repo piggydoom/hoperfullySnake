804e1926cf52197a7eb37574540f713bc36b11e1
artist piggy <artistpiggy@gmail.com>
2025-09-18 10:37:45 +1000

    bug fixed

--- main.js
+++ main.js
@@ -1,38 +1,39 @@
   const field = document.getElementById('playField');
   const ctx = field.getContext('2d');
   const cellSize = 10; 
-  ctx.fillStyle = '#007bff';
-  ctx.fillRect(200, 200, 10, 10);
   var posX = (field.clientWidth / 2);
   var posY = (field.clientWidth / 2);
-
   let direction = null;
+  ctx.fillStyle = '#007bff';
+  ctx.fillRect(200, 200, 10, 10);
 
   function moveSnake(direction){
 
-  
-  
-    ctx.clearRect(cellSize, cellSize, posX, posY);
+  // ctx.clearRect(cellSize, cellSize, posX, posY);
   
   if(direction === "up"){
     posY -= 10;
     ctx.fillRect(posX, posY, 10, 10);
+    ctx.clearRect(posX, (posY + 10), cellSize, cellSize)
   };
 
   if(direction === "down"){
     posY += 10;
     ctx.fillRect(posX, posY, 10, 10);
+    ctx.clearRect(posX, (posY - 10), cellSize, cellSize)
   };
 
   if(direction === "left"){
    
     posX -= 10;
     ctx.fillRect(posX, posY, 10, 10);
+    ctx.clearRect((posX + 10), posY, cellSize, cellSize)
   };
 
   if(direction === "right"){
     posX += 10;
     ctx.fillRect(posX, posY, 10, 10);
+    ctx.clearRect((posX - 10), posY, cellSize, cellSize)
   };
 
   
