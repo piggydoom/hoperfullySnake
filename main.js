3cd8575cc482388a512bf3621c1584ea9105b59f
artist piggy <artistpiggy@gmail.com>
2025-09-18 13:25:14 +1000

    new sprites

--- main.js
+++ main.js
@@ -1,44 +1,60 @@
   const field = document.getElementById('playField');
   const ctx = field.getContext('2d');
-  const cellSize = 10; 
+  const cS = 10; //cellSize
+  const randomX = Math.round(((Math.random(1, field.clientWidth)) * 200));
+  const randomY = Math.round(((Math.random(1, field.clientHeight)) * 200));
+  
+
   var posX = (field.clientWidth / 2);
   var posY = (field.clientWidth / 2);
+
   let direction = null;
-  ctx.fillStyle = '#007bff';
-  ctx.fillRect(200, 200, 10, 10);
+  let apple = new Image();
+  let snake = new Image();
+
+  snake.src = 'assets/snake.png';
+  apple.src = 'assets/apple.png';
+
+  // ctx.fillStyle = '#007bff';
+  // ctx.fillRect(200, 200, cS, cS);
+
+    
+    snake.src = 'assets/snake.png';
+  
+      snake.onload = () => {
+        ctx.drawImage(snake, posX, posY); // Draws the image at (10,10) with width 150 and height 100
+    };
 
   function moveSnake(direction){
-
-  // ctx.clearRect(cellSize, cellSize, posX, posY);
-  
+ 
   if(direction === "up"){
-    posY -= 10;
-    ctx.fillRect(posX, posY, 10, 10);
-    ctx.clearRect(posX, (posY + 10), cellSize, cellSize)
+    posY -= cS;
+    ctx.drawImage(snake, posX, posY);
+    ctx.clearRect(posX, (posY + cS), cS, cS)
   };
 
   if(direction === "down"){
-    posY += 10;
-    ctx.fillRect(posX, posY, 10, 10);
-    ctx.clearRect(posX, (posY - 10), cellSize, cellSize)
+    posY += cS;
+    ctx.drawImage(snake, posX, posY);
+    ctx.clearRect(posX, (posY - cS), cS, cS)
   };
 
   if(direction === "left"){
-   
-    posX -= 10;
-    ctx.fillRect(posX, posY, 10, 10);
-    ctx.clearRect((posX + 10), posY, cellSize, cellSize)
+    posX -= cS;
+    ctx.drawImage(snake, posX, posY);
+    ctx.clearRect((posX + cS), posY, cS, cS)
   };
 
   if(direction === "right"){
-    posX += 10;
-    ctx.fillRect(posX, posY, 10, 10);
-    ctx.clearRect((posX - 10), posY, cellSize, cellSize)
+    posX += cS;
+    ctx.drawImage(snake, posX, posY);
+    ctx.clearRect((posX - cS), posY, cS, cS)
   };
 
-  
-
-  console.log(posX + "-" + posY)
+  if(posX > field.clientWidth){ console.log("lost")};
+  if(posX < 0){ console.log("lost")};
+  if(posY > field.clientHeight){ console.log("lost")};
+  if(posY < 0){ console.log("lost")};
   };
 
   document.addEventListener("keypress", function(event) {
@@ -47,8 +63,21 @@
     if (event.key === "s") { direction = "down"; }
     if (event.key === "a") { direction = "left"; }
     if (event.key === "d") { direction = "right"; }
-
+    if (event.key === "=") {console.log(posX + "-" + posY)}
+      
   });
 
   setInterval(() => 
     moveSnake(direction), 200);
+
+  //FOOD
+  appleGen();
+  
+  function appleGen(){
+    // ctx.fillStyle = '#ff0000ff';
+    // ctx.fillRect(randomX, randomY, cS, cS);  
+      apple.onload = () => {
+        ctx.drawImage(apple, randomX, randomY); // Draws the image at (10,10) with width 150 and height 100
+    };
+
+  };
